d58589ae06d903b467b620140d8853537cd46a12
MHWall <31493445+MHWall@users.noreply.github.com>
2018-06-25 13:51:17 -0400

    Start of simple contact list

---
+++ Simple Console Contacts/Contact.cpp
@@ -0,0 +1,69 @@
+#include "stdafx.h"
+#include "Contact.h"
+#include "stdafx.h"
+#include <cstdio>
+#include <iostream>
+#include <fstream>
+
+
+using namespace std;
+
+
+
+Contact::Contact(string first, string last, string phone, string email, string home) {
+	firstName = first;
+	lastName = last;
+	phoneNum = phone;
+	emailAdd = email;
+	homeAdd = home;
+}
+
+string Contact::getFirstName() {
+	return firstName;
+}
+
+string Contact::getLastName() {
+	return lastName;
+}
+
+string Contact::getPhoneNum() {
+	return phoneNum;
+}
+
+string Contact::getEmailAdd() {
+	return emailAdd;
+}
+
+string Contact::getHomeAdd() {
+	return homeAdd;
+}
+
+void Contact::setFirstName(string first) {
+	firstName = first;
+}
+
+void Contact::setLastName(string last) {
+	lastName = last;
+}
+
+void Contact::setPhoneNum(string phone) {
+	phoneNum = phone;
+}
+
+void Contact::setEmailAdd(string email) {
+	emailAdd = email;
+}
+
+void Contact::setHomeAdd(string home) {
+	homeAdd = home;
+}
+
+void Contact::displayContact() {
+	cout << firstName << " " << lastName << "\n" << phoneNum << "\n" << emailAdd << "\n" << homeAdd << "\n" << endl;
+}
+
+void Contact::writeToBook() {
+	ofstream outfile;
+	outfile.open("contactbook.txt", ios::app);
+	outfile << firstName << " " << lastName << "\n" << phoneNum << "\n" << emailAdd << "\n" << homeAdd << "\n" << endl;
+}
---
+++ Simple Console Contacts/Contact.h
@@ -0,0 +1,28 @@
+#ifndef CONTACT_H
+#define CONTACT_H
+
+#include <string>
+using namespace std;
+class Contact
+{
+	private:
+		string firstName, lastName, phoneNum, emailAdd, homeAdd;
+	public:
+		Contact(string first, string last, string phone, string email, string home);
+		string getFirstName();
+		string getLastName();
+		string getPhoneNum();
+		string getEmailAdd();
+		string getHomeAdd();
+		void setFirstName(string first);
+		void setLastName(string last);
+		void setPhoneNum(string phone);
+		void setEmailAdd(string email);
+		void setHomeAdd(string home);
+		void displayContact();
+		void writeToBook();
+
+};
+
+#endif
+
